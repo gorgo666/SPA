1a583c9053a727ef7e131da73156a25e0503ca90
gorgo666 <grzegorz.wiecek.sluzbowy@gmail.com>
2020-02-26 17:57:11 +0100

    initial project

---
+++ src/views/rooms.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import $ from 'jquery';
+import { roomsService } from '../common/rooms-service';
+
+export const rooms = () => {
+  const fragment = $(new DocumentFragment());
+
+  fragment
+    .append('<h2>Rooms</h2>')
+    .append('<p>Lorem ipsum dolor sit amet...</p>');
+
+  return fragment;
+};
